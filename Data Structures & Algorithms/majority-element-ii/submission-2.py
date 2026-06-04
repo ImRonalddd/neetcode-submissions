514c3a2aeda3b50b01fdd44e27b2b43ba4194be9
ImRonalddd <157067197+ImRonalddd@users.noreply.github.com>
2026-06-03 18:50:51 -0700

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        n1, n2 = None, None
+        c1, c2 = 0, 0
+        for n in nums:
+            if n == n1:
+                c1 += 1
+            elif n == n2:
+                c2 += 1
+            elif c1 == 0:
+                n1, c1 = n, 1
+            elif c2 == 0:
+                n2, c2 = n, 1
+            else:
+                c1, c2 = c1-1, c2-1
+        return [c for c in (n1, n2) if nums.count(c) > len(nums) // 3]
