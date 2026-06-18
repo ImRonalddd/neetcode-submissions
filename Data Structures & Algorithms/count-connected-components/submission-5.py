33f334fba119fb6aada8df8234624a96ccca1f7f
ImRonalddd <157067197+ImRonalddd@users.noreply.github.com>
2026-06-18 12:47:36 -0700

    Add: count-connected-components - submission-5

---
+++ Data Structures & Algorithms/count-connected-components/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        comp = n
+        parent = list(range(n))
+
+        def find(x):
+            while x != parent[x]:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                comp -= 1
+                parent[ra]=parent[rb]
+        return comp
